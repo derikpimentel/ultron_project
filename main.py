56bab9638c7578615f34cd5a9590711b3ff032d0
derikpimentel <derikpimentel@hotmail.com.br>
2024-10-30 14:24:43 -0300

    add main screen

--- main.py
+++ main.py
@@ -7,22 +7,23 @@ from tkinter.ttk import Progressbar
 class Application:
     def __init__(self, master=None):
         self.master = master # Faz a passagem da variável "root"
+        self.master.title("Ultron") # Altera o título da janela
         self.master.geometry("300x75") # Define as dimensões da janela (L x A)
         self.master.overrideredirect(True) # Remove a barra superior
 
-        # Estruturando os widgets da janela
-        self.container_load_screen = Frame(self.master)
-        self.container_load_screen.pack(pady=10) # O método "pack()" organiza em bloco
-        # início do "container_load_screen"
-        self.label_notice = Label(self.container_load_screen)
+        # Estruturando os widgets da janela de carregamento
+        self.loading_screen = Frame(self.master)
+        self.loading_screen.pack(pady=10) # O método "pack()" organiza em bloco
+        # início do "loading_screen"
+        self.label_notice = Label(self.loading_screen)
         self.label_notice["text"] = "Carregando, por favor aguarde..."
         self.label_notice.pack()
-        self.progress_bar = Progressbar(self.container_load_screen)
+        self.progress_bar = Progressbar(self.loading_screen)
         self.progress_bar["orient"] = "horizontal"
         self.progress_bar["mode"] = "determinate"
         self.progress_bar["length"] = 250
         self.progress_bar.pack()
-        # final do "container_load_screen"
+        # final do "loading_screen"
 
         self.center_window() # Centraliza a janela após criar todo conteúdo da tela
 
@@ -63,16 +64,46 @@ class Application:
             time.sleep(0.05)
             self.update_progress(i) # Atualiza a barra de progresso
         time.sleep(1) # Paralisa a execução (Seg)
-        self.close() # Fecha a tela de carregamento após a operação
+        self.loading_main_screen() # Carrega a estrutura da janela principal
 
     # Função que atualiza a barra de progresso
     def update_progress(self, value):
         self.progress_bar['value'] = value
         self.master.update_idletasks()
 
-    # Função que encerra a janela
-    def close(self):
-        self.master.destroy() # Fecha a janela e encerra a aplicação
+    # Função que inicializa a janela principal do sistema
+    def loading_main_screen(self):
+        self.master.withdraw() # Esconde a janela para fazer as modificações
+        self.loading_screen.destroy() # Apaga o widget da tela de carregamento
+        #self.master.geometry("") # Retorna para o redmensionamento automático
+        self.master.overrideredirect(False) # Devolve a barra superior
+        self.master.resizable(False, False) # Desabilita o redimensionamento da janela
+
+        # Estruturando os widgets da janela principal
+        self.main_screen = Frame(self.master)
+        self.main_screen.pack()
+        # início do "main_screen"
+        self.container_header = Frame(self.main_screen)
+        self.container_header.pack()
+        self.label_title = Label(self.container_header)
+        self.label_title["text"] = "Inventário de máquina"
+        self.label_title.pack()
+
+        self.container_main = Frame(self.main_screen)
+        self.container_main.pack()
+        self.label_description = Label(self.container_main)
+        self.label_description["text"] = "Dados do equipamento"
+        self.label_description.pack()
+
+        self.container_footer = Frame(self.main_screen)
+        self.container_footer.pack()
+        self.label_info = Label(self.container_footer)
+        self.label_info["text"] = "Desenvolvido por Derik B. Pimentel"
+        self.label_info.pack()
+        # início do "main_screen"
+
+        self.center_window()
+        self.master.deiconify() # Mostra a janela novamente
 
 # Estrutura de execução
 if __name__ == "__main__":
